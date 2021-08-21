6a047d6fc4c77467c917bf4e81da793dcdd77925
NithinraoOdela <65495762+NithinraoOdela@users.noreply.github.com>
2021-08-22 00:15:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+// import ChatFeed from './components/ChatFeed';
+
+const App = () => {
+    
+  
+    return (
+      <ChatEngine
+        height="100vh"
+        projectID="a2146a30-8398-4d4b-abf5-765d90d7bcf7"
+        userName="mrkk"
+        userSecret="Nithinrao@123"
+        
+        
+      />
+    );
+  };
+
+  export default App;
